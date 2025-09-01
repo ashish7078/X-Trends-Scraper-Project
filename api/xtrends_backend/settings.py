f5375c6055a6e57a9432489f03dbbd468d1a296d
ashish7078 <ashishkumar98961@gmail.com>
2025-09-01 22:47:19 +0530

    added cors allowed origin

--- api/xtrends_backend/settings.py
+++ api/xtrends_backend/settings.py
@@ -139,6 +139,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 # ✅ CORS Settings
 CORS_ALLOWED_ORIGINS = [
+    "https://x-trends-scraper-project.vercel.app/",
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "http://localhost:3000",
